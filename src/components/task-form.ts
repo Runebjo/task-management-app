60d1a5f74000f4a986f25d6094dd188b0f561d01
Runebjo <rune.bjorsnos@gmail.com>
2024-08-05 14:08:43 +0200

    fix TypeScript error

--- src/components/task-form.ts
+++ src/components/task-form.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 import { taskStore } from '../store/task-store';
 
 @customElement('task-form')
@@ -7,6 +7,7 @@ export class TaskForm extends LitElement {
     static properties = {
         taskTitle: { type: String }
     };
+    taskTitle: string;
 
     constructor() {
         super();
